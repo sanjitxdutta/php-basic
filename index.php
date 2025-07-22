e06ec0e6d615e040f2626aa15a0259b9a2b59d89
sanjitxdutta <duttasanjit691@gmail.com>
2025-07-22 12:54:17 +0530

    Operators in php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
         This is my first php website.
         <?php
             echo "Hello world and this is printed using php";
-
+            echo "<br>";  //new-line
             //  Single line comments
 
             /*THis
@@ -19,12 +19,89 @@
             multi-line
             comment  */
 
-            $variable1 = 34;
-            $variable2 = 25;
+            //  variable in php
+
+            $variable1 = 5;
+            $variable2 = 2;
 
             echo $variable1;
             echo $variable2;
             echo $variable1 + $variable2;
+
+            // Operators in php
+            // Arithmetic operators
+            echo "<br>";  //new-line
+            echo "The value of variable1 + variable2 is ";
+            echo $variable1 + $variable2;
+
+            echo "<br>";  //new-line
+            echo "The value of variable1 - variable2 is ";
+            echo $variable1 - $variable2;
+
+            echo "<br>";  //new-line
+            echo "The value of variable1 * variable2 is ";
+            echo $variable1 * $variable2;
+
+            echo "<br>";  //new-line
+            echo "The value of variable1 / variable2 is ";
+            echo $variable1 / $variable2;
+            
+            // Assignment operators
+            echo "<br>";
+            $newVar = $variable1;
+            $newVar += 1;
+            $newVar -= 1;
+            $newVar *= 2;
+            $newVar /= 2;
+
+            echo "The value of new variable is ";
+            echo $newVar;
+            
+            // Comparison operators
+            // echo "<h1> Comparison operators </h1>"
+            echo "<br>";
+            echo "The value of 1==4 is ";
+            echo var_dump(1==4);
+
+            echo "<br>";
+            echo "The value of 1!=4 is ";
+            echo var_dump(1!=4);
+
+            echo "<br>";
+            echo "The value of 1>=4 is ";
+            echo var_dump(1>=4);
+
+            echo "<br>";
+            echo "The value of 1<=4 is ";
+            echo var_dump(1<=4);
+
+            echo "<br>";
+            echo "The value of 1>4 is ";
+            echo var_dump(1>4);
+
+            echo "<br>";
+            echo "The value of 1<4 is ";
+            echo var_dump(1<4);
+
+            // Increment/Decrement operators
+            echo "<br>";
+            echo "The post incremented value of newVar++ is ";
+            echo $newVar++;
+
+            echo "<br>";
+            echo "The post decremented value of newVar-- is ";
+            echo $newVar--;
+
+            echo "<br>";
+            echo "The pre decremented value of ++newVar is ";
+            echo ++$newVar;
+
+            echo "<br>";
+            echo "The pre decremented value of --newVar is ";
+            echo --$newVar;
+
+            // Logical operators
+
         ?>
     </div>
 </body>
